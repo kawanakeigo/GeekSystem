c234e3bbeca4aa033c22a0a22e215a2a64773b72
kawanakeiware <kawanakeiware@localhost>
2025-10-01 17:04:32 +0900

    バリデーションエラーが表示されるよう修正しました

--- demo/src/main/java/com/example/demo/form/AdminForm.java
+++ demo/src/main/java/com/example/demo/form/AdminForm.java
@@ -9,11 +9,11 @@ import jakarta.validation.constraints.Size;
 public class AdminForm {
 	private Long id;
     @NotBlank(message = "名前は必須です")
-    @Size(max = 50)
+    @Size(max = 50, message = "50文字以内で入力してください")
     private String name;
     @NotBlank(message = "メールアドレスは必須です")
     @Email(message = "メールアドレスの形式で入力してください")
-    @Size(max = 50)
+    @Size(max = 50 , message = "50文字以内で入力してください")
     private String email;
     private String password; 
     @NotNull(message = "役職を選択してください")
