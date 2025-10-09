fc8a559c41cf226a20b9908405cdd6c8fb0c2c51
kawanakeiware <kawanakeiware@localhost>
2025-10-09 13:23:52 +0900

    権限のデータ追加するために実装

---
+++ demo/src/main/java/com/example/demo/entity/Authorities.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "authorities")
+public class Authorities {
+	
+	@Id
+	private Long Id;
+	private String name;
+    
+    //以下getter・setter
+	public Long getId() {
+		return Id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+
+
+}
